444c1580fef5cd874d867c2f98766f814c399e46
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-08-22 19:58:41 +0200

    changed function so that it gets info from jsonplaceholder

--- ajaxtest.js
+++ ajaxtest.js
@@ -5,10 +5,13 @@ document.getElementById("clickBtn").addEventListener('click', function() {
 function changeTitle() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200){
-            document.getElementById("box").innerHTML = this.responseText;
+        if(this.readyState === 4 && this.status === 200){
+            console.log(this.responseText);
+        } else if(this.readyState === 4){
+            console.log("could not find data");
+
         }
     };
-    xhttp.open("GET", "text.txt", true);
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/", true);
     xhttp.send();
 }
